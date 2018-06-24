93aa8cc56f48d7221f430fa4bef734d592eab98c
Chantal Wallace <theteatreehouse@gmail.com>
2018-06-24 19:37:39 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+def using_push(array,string)
+  array.push(string)
+end 
+# adds string onto the end of the array
+
+def using_unshift(array,string)
+  array.unshift(string)
+end 
+# adds string tot eh beginning of the array
+
+def using_pop(array)
+  array.pop 
+end
+# removes the last element in the array 
+
+def pop_with_args(array)
+  array.pop(2)
+end
+# removes last 2 elements of the array
+ 
+def using_shift(array)
+  array.shift
+end
+# removes first element of the array
+
+def shift_with_args(array)
+  array.shift(2)
+end
